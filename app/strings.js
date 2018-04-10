6457918358aa3fff19bf5b1162b7220eb10f4d69
James Logue <logueja@gmail.com>
2018-04-10 09:42:24 -0600

    Complete problems 1 & 2 of strings.js

--- app/strings.js
+++ app/strings.js
@@ -2,11 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    const chars = str.split('')
 
+    return chars.reduce((acc, char, idx) => {
+      if (chars[idx+amount] === char) {
+        return acc
+      }
+      return [...acc, char]
+    }, []).join('')
   },
 
   wordWrap: function(str, cols) {
+    const split = str.split(' ')
+    let shift = 0
 
+    return split.reduce((acc, string, idx) => {
+      if (string.length === 1 && idx > 0) {
+        shift++
+        acc[idx - shift] += ` ${string}`
+        return acc
+      } 
+      return [...acc, string]
+    },[]).join('\n')
   },
 
   reverseString: function(str) {
